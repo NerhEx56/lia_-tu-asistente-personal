2288b44aee5248dc1afbc9cc3988a2e180ffac37
NerhEx56 <99098474+NerhEx56@users.noreply.github.com>
2025-07-18 12:53:14 -0300

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,11 +1,11 @@
 import { GoogleGenAI, Chat } from "@google/genai";
 
 export function createChatSession(systemInstruction: string): Chat {
-  if (!process.env.API_KEY) {
+  if (!process.env.AIzaSyBncldgks27JPgc6eBgZD2xvekLid8JZKE) {
       throw new Error("API key not found. Please set the API_KEY environment variable.");
   }
 
-  const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
+  const ai = new GoogleGenAI({ apiKey: process.env.AIzaSyBncldgks27JPgc6eBgZD2xvekLid8JZKE });
   const model = 'gemini-2.5-flash';
   const chat = ai.chats.create({
     model: model,
